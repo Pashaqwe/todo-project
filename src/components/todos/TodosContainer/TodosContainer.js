b0ee84b8594ca1d1d9837875a3a6dffb125a24dc
Pavel Votchinnikov <pashakostilev@gmail.com>
2020-12-15 17:05:53 +0300

    create new component with logics

---
+++ src/components/todos/TodosContainer/TodosContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import API from '../api'
+import TodosComponent from '../TodosComponent'
+
+class TodosContainer extends Component {
+  state = {
+    todos: [],
+  }
+
+  componentDidMount() {
+    API.get(`todos`).then((response) => {
+      this.setState({
+        todos: response.data,
+      })
+    })
+  }
+
+  onCreateTodo = (name) => {
+    console.log(name)
+  }
+  render() {
+    return (
+      <TodosComponent
+        todos={this.state.todos}
+        onCreateTodo={this.onCreateTodo}
+      />
+    )
+  }
+}
+
+export default TodosContainer
