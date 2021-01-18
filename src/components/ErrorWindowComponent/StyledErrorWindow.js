0a6d90a45076dc833bed73b3e58a9510ddc8bfa2
Pavel Votchinnikov <pashakostilev@gmail.com>
2021-01-18 19:10:38 +0300

    Made the layout more responsive

--- src/components/ErrorWindowComponent/StyledErrorWindow.js
+++ src/components/ErrorWindowComponent/StyledErrorWindow.js
@@ -3,21 +3,14 @@ import styled from 'styled-components'
 export const ErrorWindow = styled.div`
   display: ${(props) => (props.styledServerError ? 'box' : 'none')};
   background-color: #ffffff;
-  width: 80%;
-  margin: 0 10%;
-  height: 100px;
   border-radius: 6px;
-  position: absolute;
-  top: 200px;
+  position:relative;
+  padding:5px 5px 5px 15px;
+  top: 100px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
 `
 
-export const ErrorWindowHeding = styled.h3`
-  margin-left: 20px;
-`
-export const ErrorWindowDescription = styled.p`
-  margin-left: 20px;
-`
+
 
 export const CloseButton = styled.button`
   position absolute;
--- src/components/TodoListComponent/StyledTodoListComponent.js
+++ src/components/TodoListComponent/StyledTodoListComponent.js
@@ -3,12 +3,14 @@ import styled from 'styled-components'
 export const StyledList = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  margin: 0 auto;
+  align-content: flex-start;
+  justify-content: space-evenly;
 `
 
 export const StyledCard = styled.li`
   height: 150px;
   width: 20%;
+  min-width:200px;
   list-style-type: none;
   background-color: ${(props) =>
     props.isDisabled ? 'rgb(128,128,128,0.5)' : '#FFFFFF'};
@@ -29,12 +31,25 @@ export const StyledDate = styled.p`
   position: absolute;
   left: 15px;
   bottom: 15px;
-  margin-bottom:10px;
-  margin-left:10px;
-  font-size:13px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  font-size: 13px;
 `
 
-export const StyledButton = styled.button`
+export const ChangeButton = styled.button`
+  position: absolute;
+  right: 50px;
+  bottom: 15px;
+  border-radius: 6px;
+  outline: none;
+  border: none;
+  background-color: rgba(255, 255, 255, 0);
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export const TrashButton = styled.button`
   position: absolute;
   right: 15px;
   bottom: 15px;
@@ -45,4 +60,23 @@ export const StyledButton = styled.button`
   &:hover {
     cursor: pointer;
   }
+`
+
+export const ConfirmButton = styled.button`
+  position: absolute;
+  right: 15px;
+  border-radius: 6px;
+  outline: none;
+  border: none;
+  background-color: rgba(255, 255, 255, 0);
+  &:hover {
+    cursor: pointer;
+  }
+`
+export const StyledInput = styled.input`
+  height: 20px;
+  width: 87%;
+  border-radius: 6px;
+  outline: none;
+  border: 2px solid #a3a3cd;
 `
